9869d0ece342263fd62e4e0e024d81ebeae477c3
gnvsai <venkata.sai@zemosolabs.com>
2022-08-03 16:35:24 +0530

    updated

---
+++ sonar-jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+record=$(curl -s $auth "https://sonarcloud.io/api/measures/component?component=gnvsai&metricKeys=reliability_rating")
+value=$(echo $record | jq -r '.component.measures[0].value')
+echo $value
+
+export ZOHO_TOKEN=$(curl -X POST "https://accounts.zoho.com/oauth/v2/token?client_id=1000.WRSS1BW7HC6UTSKDPPFWTKI6PO9AEZ&client_secret=64e2636efb747eb35fdd01cf5a15cc25b4b6bcf0cd&redirect_uri=http://application_name.com/&grant_type=refresh_token&scope=ZohoCliq.Channels.CREATE,ZohoCliq.Channels.READ,ZohoCliq.Channels.UPDATE,ZohoCliq.Channels.DELETE,ZohoCliq.Webhooks.CREATE&refresh_token=1000.70c6e040ff61d85a86a2373850af9137.e10bb573ab5d8c909e4fa77ce8382963" | jq '.["access_token"]' | sed 's/\"//g')
+
+A="1.0"
+B="2.0"
+C="3.0"
+D="4.0"
+
+if [[ "$value" = "$A" ]]; then
+python3 /var/lib/jenkins/workspace/github-webhook/cliq-a.py
+elif [[ "$value" = "$B" ]]; then    
+python3 /var/lib/jenkins/workspace/github-webhook/cliq-b.py
+elif [[ "$value" = "$C" ]]; then
+python3 /var/lib/jenkins/workspace/github-webhook/cliq-c.py
+elif [[ "$value" = "$D" ]]; then 
+python3 /var/lib/jenkins/workspace/github-webhook/cliq-d.py
+else
+python3 /var/lib/jenkins/workspace/github-webhook/cliq-e.py
+fi
